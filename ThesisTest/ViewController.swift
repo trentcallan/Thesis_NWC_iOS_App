dc8cadaada4ac831c1c3d73afb7005e7085adc6f
Trent Callan <trentcallan@Trents-MacBook-Pro-2.local>
2018-09-09 14:42:50 -0700

    Initial Commit

---
+++ ThesisTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThesisTest
+//
+//  Created by Trent Callan on 9/9/18.
+//  Copyright © 2018 Trent Callan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
